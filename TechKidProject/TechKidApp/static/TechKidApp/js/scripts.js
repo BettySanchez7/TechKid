20c5deaff4ab454737b0174dca39e7dace0d8f0a
BettySanchez7 <bety.sanchez.diaz@gmail.com>
2021-08-16 02:24:32 -0500

    Correción de templates log in y registro

--- TechKidProject/TechKidApp/static/TechKidApp/js/scripts.js
+++ TechKidProject/TechKidApp/static/TechKidApp/js/scripts.js
@@ -1,4 +1,37 @@
-function validation(){
+
+(function($) {
+  const openEls = document.querySelectorAll("[data-open]");
+const closeEls = document.querySelectorAll("[data-close]");
+const isVisible = "is-visible";
+
+for (const el of openEls) {
+  el.addEventListener("click", function() {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
+
+for (const el of closeEls) {
+  el.addEventListener("click", function() {
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+  });
+}
+
+document.addEventListener("click", e => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+document.addEventListener("keyup", e => {
+  // if we press the ESC
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+})(jQuery);
+
+/*function validation(){
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var tel = document.getElementById("tel").value;
@@ -42,4 +75,4 @@ function validation(){
     alert("El formulario se envió exitosamente")
     return true;
 
-}
+} */
